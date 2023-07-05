458ea3e55dac915b16ad4fe5a69eb7696131dd17
Juan Carlos Carmona Calvo <43755989+juanky201271@users.noreply.github.com>
2023-07-05 19:39:22 +0200

    Revert "Add commands on launch tests"

--- android/app/src/androidTest/java/org/ZingoLabs/Zingo/IntegrationTestSuite.kt
+++ android/app/src/androidTest/java/org/ZingoLabs/Zingo/IntegrationTestSuite.kt
@@ -7,6 +7,6 @@ import org.junit.runners.Suite.SuiteClasses
 
 @RunWith(Categories::class)
 @IncludeCategory(IntegrationTest::class)
-@SuiteClasses(CommandsOnLaunch::class)
+@SuiteClasses(ExecuteAddressesTest::class)
 class IntegrationTestSuite {
 }
